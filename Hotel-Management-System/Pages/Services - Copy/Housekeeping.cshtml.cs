f19d419248ed3863e3e5d15d410b52d1074892fe
PC <galalqassas0@gmail.com>
2023-12-16 15:31:01 +0200

    Add housekeeping and maintenance

---
+++ Hotel-Management-System/Pages/Services - Copy/Housekeeping.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel_Management_System.Pages.Services___Copy
+{
+    public class HousekeepingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
